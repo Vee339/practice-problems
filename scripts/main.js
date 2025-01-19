fcdaefcc20590dd6e2ec1e360617efcb5ff74b5a
Vee339 <81091817+Vee339@users.noreply.github.com>
2025-01-19 03:09:34 -0500

    Second problem is solved

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,13 @@
 // Problem #1 - Create a "Hello World" program
 
-let username = prompt("What is your name?");
-console.log(`Hello, ${username}, nice to meet you!`);
+// let username = prompt("What is your name?");
+// console.log(`Hello, ${username}, nice to meet you!`);
+// ---------- The above problem is commented because there is an alert which will pop up every time we refresh the page
+
+// Problem #2 -
+// Create a program that prompts for an input string and displays output that shows the input string and the number of characters the string contains.
+
+// let string = prompt("Write anything that you want to", "Example String");
+let string = "Hello, this is the example string.";
+console.log(string);
+console.log(`"${string}" has ${string.length} characters.`);
