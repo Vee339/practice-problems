abf62d169f780b9ebbe460ce197de73be0d5aa02
Vee339 <81091817+Vee339@users.noreply.github.com>
2025-01-19 03:34:19 -0500

    Three more problems are solved

--- scripts/main.js
+++ scripts/main.js
@@ -11,3 +11,57 @@
 let string = "Hello, this is the example string.";
 console.log(string);
 console.log(`"${string}" has ${string.length} characters.`);
+
+// Problem #3 -
+// Create a program that prompts for a quote and an author. Display the quotation and author as shown in example output.
+// Example output - Obi-Wan Kenobi says, "These are not the droids you're looking for."
+
+// let quote = prompt("What is the quote?");
+// let author = prompt("Who said it?");
+
+// console.log(`${author} says, "${quote}"`);
+
+// Problem #4 -
+// Create a Mad Libs. Take a verb, noun, adverb and adjective from the user and make a story out of it.
+
+/*
+const noun = prompt("What is the noun?");
+const verb = prompt("What is the verb?");
+const adjective = prompt("What is the adjective?");
+const adverb = prompt("What is the adverb?");
+
+console.log(
+  `Do you ${verb} your ${adjective} ${noun} ${adverb}? That's hilarious!`
+);
+
+*/
+
+// Problem #5 - Take two numbers from the user. Do the sum, multiplication, difference and quotient of those numbers.
+
+/*
+const firstNum = Number(prompt("Enter your first number, please."));
+const secondNum = Number(prompt("Enter your second number."));
+
+const sum = firstNum + secondNum;
+const diff = firstNum - secondNum;
+const multiplication = firstNum * secondNum;
+const quotient = firstNum / secondNum;
+
+console.log(sum, diff, multiplication, quotient);
+*/
+
+// Problem #6 - Create a program that determines how many years you have left until retirement and the year you can retire. It should prompt for your current age and age you want to retire and display the output.
+
+/*
+let age = Number(prompt("What is your current age?"));
+let retirementAge = Number(prompt("At what age would you like to retire?"));
+
+const yearsDiff = retirementAge - age;
+const today = new Date();
+const currentYear = today.getFullYear();
+const retirementYear = currentYear + yearsDiff;
+
+console.log(
+  `You have ${yearsDiff} years left until you can retire. It's ${currentYear}, so you can retire in ${retirementYear}.`
+);
+*/
